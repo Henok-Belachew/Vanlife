340a5373b7a821d4d99b84c36439531a5582bb38
Henok Belachew <henokbelachewstu@gmail.com>
2023-05-12 18:43:41 +0300

    simpliefied

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default function App () {
         <div className="w-[480px] mx-auto bg-[#FFF7ED] relative">
             <BrowserRouter>
         <nav className="flex justify-between items-center bg-[#FFF7ED] py-8 px-10">
-            <Link to="/"><img className="w-[100px]" src={logo} alt="" /></Link>   
+            <Link to="/"><img className="w-[110px]" src={logo} alt="" /></Link>   
             <ul className="flex gap-5 text-sm">
                 <Link className="" to="/host"><li>Hosts</li></Link>
                 <Link to="/vans"><li>Vans</li></Link>   
--- src/pages/host.jsx
+++ src/pages/host.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import img from '../images/van1.png'
-// import Data from '../API';
+import Data from '../API';
 
 
-// export function Van (props) {
-//     return (
-//         <div className='van-card'>
-//             <img src={props.img} alt="" />
-//             <div className="van-dt">
-//                 <div className="van-left">
-//                     <h2>{props.name}</h2>
-//                     <span>${props.price}/day</span>
-//                 </div>
+export function Van (props) {
 
-//                 <span>Edit</span>
-//             </div>
-//         </div>
-//     )
-// }
+    if (props.listed == true) {
+    return (
+        <div className='flex flex-row p-3 bg-white rounded-sm'>
+            <img src={props.img} className='w-[65px] rounded-md'  alt="" />
+            <div className="flex flex-row justify-between items-center w-full ml-2 text-sm">
+                <div className="">
+                    <h2 className='font-bold '>{props.name}</h2>
+                    <span>{props.price}/day</span>
+                </div>
+
+                <span className='cursor-pointer'>Edit</span>
+            </div>
+        </div>
+    )
+    }
+
+}
 
 export default function Host () {
 
-    // const all_vans = Data.map((a_van)=> {
-    //     return (<Van 
-    //         name = {a_van.name} 
-    //         price = {a_van.price}
-    //         img = {a_van.imageUrl}
-    //         />)
+    const all_vans = Data.map((a_van)=> {
+        return (<Van 
+            name = {a_van.name} 
+            price = {a_van.price}
+            img = {a_van.imageUrl}
+            listed = {a_van.listed}
+            />)
             
-    // })
+    })
 
     
     return (
@@ -46,7 +51,7 @@ export default function Host () {
                 <h1 className='text-[30px] py-2  font-bold'>Welcome!</h1>
                 <div className='flex-row flex justify-between text-sm'>
                     <span className=''>Income last <span className='underline font-semibold'>30 days</span></span>
-                    <span>Details</span>
+                    <span className='cursor-pointer'>Details</span>
                 </div>
                 <h1 className='text-[32px] py-3 font-bold'>$2,260</h1>
 
@@ -54,51 +59,16 @@ export default function Host () {
             </div>
             <div className='flex justify-between flex-row py-5 bg-orange-200 px-10'>
                     <h2 className='font-bold text-lg'>Review score 5.0<span className='font-light'>/5</span></h2>
-                    <span className='text-sm'>Details</span>
+                    <span className='text-sm cursor-pointer'>Details</span>
             </div>
             <div className="px-10 my-5">
-                <h2 className='flex flex-row justify-between font-bold text-lg'>Your listed vans <span className='text-sm font-normal'>View all</span></h2>
+                <h2 className='flex flex-row justify-between font-bold text-lg'>Your listed vans <span className='text-sm font-normal cursor-pointer'>View all</span></h2>
 
                 <div className="list my-5 flex flex-col gap-3">
 
-                    {/* List-item-1 */}
-                    <div className='flex flex-row p-3 bg-white rounded-sm'>
-                        <img src={img} className='w-[65px] rounded-md'  alt="" />
-                        <div className="flex flex-row justify-between items-center w-full ml-2 text-sm">
-                            <div className="">
-                                <h2 className='font-bold '>Modest Explorer</h2>
-                                <span>$60/day</span>
-                            </div>
-
-                            <span className=''>Edit</span>
-                        </div>
-                    </div>
-
-                    {/* List-item-2 */}
-                    <div className='flex flex-row p-3 bg-white rounded-sm'>
-                        <img src={img} className='w-[65px] rounded-md'  alt="" />
-                        <div className="flex flex-row justify-between items-center w-full ml-2 text-sm">
-                            <div className="">
-                                <h2 className='font-bold'>Beach Bum</h2>
-                                <span>$60/day</span>
-                            </div>
-
-                            <span className=''>Edit</span>
-                        </div>
-                    </div>
-
-                    {/* List-item-3 */}
-                    <div className='flex flex-row p-3 bg-white rounded-sm'>
-                        <img src={img} className='w-[65px] rounded-md'  alt="" />
-                        <div className="flex flex-row justify-between items-center w-full ml-2 text-sm">
-                            <div className="">
-                                <h2 className='font-bold'>Green Wonder</h2>
-                                <span>$70/day</span>
-                            </div>
-
-                            <span className=''>Edit</span>
-                        </div>
-                    </div>
+                    {
+                        all_vans
+                    }
 
                 </div>
             </div>
